037a0460d4bf518fab20a244beec12642df50090
bobbyngo <ngohuugiabao8980@gmail.com>
2021-10-14 16:51:01 -0400

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -2,8 +2,9 @@ public class Square {
     private String name;
     private int index;
 
-    public Square(){
-
+    public Square(String name, int index){
+        this.name = name;
+        this.index = index;
     }
 
     public String getName() {
