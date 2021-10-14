8709af20bb455bec7ceb5edea78e2a7ab8225e86
Patrick Liu <patrickliu@cmail.carleton.ca>
2021-10-14 16:39:50 -0400

    iteration 1

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,16 @@
 public class Square {
+    private String name;
+    private int index;
+
+    public Square(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIndex(){
+        return index;
+    }
 }
