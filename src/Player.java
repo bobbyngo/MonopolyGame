c6404afaa22f7b02028b935a12c85a44ba7dd09b
bobbyngo <ngohuugiabao8980@gmail.com>
2021-10-22 10:48:37 -0400

    Create Player class

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,81 @@
-public class Player {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Player implements RoleAPI {
+
+    private String name;
+    private HashMap<Integer, Integer> playerWallet;
+    private boolean inJail;
+    private Square currLocation;
+    private ArrayList<PrivateProperty> propertyList;
 
     //For buying property: Check if the class is Rail by: .instanceof(Rail)
     // If yes, we do not need to check the full set of color to buy rail
+
+    public Player(String name, Square currLocation){
+        this.name = name;
+        this.playerWallet = new HashMap<>();
+        this.inJail = false;
+        this.currLocation = currLocation;
+        this.propertyList = new ArrayList<>();
+
+        //Initialize Player's wallet
+        playerWallet.put(1, 10);
+        playerWallet.put(5, 10);
+        playerWallet.put(10, 10);
+        playerWallet.put(50, 10);
+        playerWallet.put(100, 10);
+        playerWallet.put(500, 10);
+    }
+
+
+    public void buyPrivateProperty() {
+
+    }
+
+    public boolean isOwningColorGroup(){
+        return false;
+    }
+
+    public boolean isInJail(){
+        return false;
+    }
+
+    @Override
+    public void addMoney(int moneyValue, int amount) {
+
+    }
+
+    @Override
+    public void removeMoney(int moneyValue, int amount) {
+
+    }
+
+    public boolean isBankrupt() {
+        return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Square getCurrLocation() {
+        return currLocation;
+    }
+
+    public ArrayList<PrivateProperty> getPropertyList() {
+        return propertyList;
+    }
+
+    public HashMap<Integer, Integer> getPlayerWallet() {
+        return playerWallet;
+    }
+
+    public void setInJail(boolean inJail) {
+        this.inJail = inJail;
+    }
+
+    public void setCurrLocation(Square currLocation) {
+        this.currLocation = currLocation;
+    }
 }
