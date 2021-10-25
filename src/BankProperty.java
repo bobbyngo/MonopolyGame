310cbcc07c037875cce742fbe5297472c20ac239
Patrick Liu <patrickliu@cmail.carleton.ca>
2021-10-25 13:45:35 -0400

    Patrick - Oct.25th - BankProperty collect money fixed

--- src/BankProperty.java
+++ src/BankProperty.java
@@ -4,7 +4,7 @@ public class BankProperty extends Square implements PropertyAPI{
      * 101163137
      */
 
-    private Bank bank;
+    //private Bank bank;
     private int taxValue;
 
     /**
@@ -15,20 +15,18 @@ public class BankProperty extends Square implements PropertyAPI{
      */
     public BankProperty(String name, int index, int taxValue) {
         super(name, index);
-        this.bank = new Bank();
+        //this.bank = new Bank();
         this.taxValue = taxValue;
     }
 
     /**
      * @author Gabriel Benni Kelley Evensen 101119814
      *
-     * Method to collect money from a player who lands on a square that is owned by the bank, and charge the player rent, giving the rent to the bank
-     * rent is 10% of the total bank property price
+     * Method removes the money from the player who lands on this BankProperty
      */
     @Override
     public void collectMoney(){
         this.getPlayersCurrentlyOn().get(0).removeMoney(taxValue);  //Removes the rent from the player who landed on the square
-        this.bank.addMoney(taxValue);                               //Adds the rent money to the bank
     }
 
     /**
