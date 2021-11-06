a00454b41249a945d687036809bf4b9ec4c1004b
1112zakaria <1112zakaria@gmail.com>
2021-11-05 20:03:26 -0400

    Made MonopolyGUIView inherit JFrame.

--- src/MonopolyGUIView.java
+++ src/MonopolyGUIView.java
@@ -11,39 +11,22 @@ import java.util.ArrayList;
 
 public class MonopolyGUIView extends JFrame{
     private Board board;
-
-    private final JFrame frame;
     private final JPanel mainPanel;
     private final GridBagLayout gb;
     private final GridBagConstraints c;
     private final ArrayList<JPanel> squares;
     private final ArrayList<JLabel> playerLabels;
 
-    private final JButton showStats;
-    private final JButton roll;
-    private final JButton buy;
-    private final JButton sell;
-    private final JButton endTurn;
-
-
     public MonopolyGUIView(){
         board = new Board();
-
-        frame = new JFrame("Monopoly Game");
         gb = new GridBagLayout();
         mainPanel = new JPanel(gb);
         c = new GridBagConstraints();
         squares = new ArrayList<>();
         playerLabels = new ArrayList<>();
-
-        showStats = new JButton();
-        roll = new JButton();
-        buy = new JButton();
-        sell = new JButton();
-        endTurn = new JButton();
     }
 
-    private void SquaresLayout(){
+    public void SquaresLayout(){
 
         for(int i = 0; i < 38; i++){
             JPanel squarePanel = new JPanel(new BorderLayout());
@@ -85,7 +68,7 @@ public class MonopolyGUIView extends JFrame{
         }
     }
 
-    private void addSquareToBoard(){
+    public void addSquareToBoard(){
         for(int i = 0; i < 10; i++){
             c.gridx = i;
             c.gridy = 0;
@@ -119,50 +102,27 @@ public class MonopolyGUIView extends JFrame{
         }
     }
 
-    private void addButtonToBoard(){
-        // Show Stats button
-        c.gridx = 2;
-        c.gridy = 2;
-        c.gridwidth = 4;
-        c.gridheight = 2;
-        gb.setConstraints(showStats, c);
-        showStats.setText("Show Stats");
-        showStats.setForeground(Color.RED);
-        // content of the action listener will be replaced with a function in Monopoly Controller to display the current player stats
-        showStats.addActionListener(e->System.out.println("hello"));
-        mainPanel.add(showStats);
-
-        // Roll Button
-        c.gridx = 4;
-        gb.setConstraints(roll, c);
-        roll.setText("Roll Dice");
-        roll.setForeground(Color.RED);
-        // content of the action listener will be replaced with a function in Monopoly Controller to display the current player stats
-        roll.addActionListener(e->System.out.println("hello"));
-        mainPanel.add(roll);
-    }
-
     public void displayGUI(){
         SquaresLayout();
         addSquareToBoard();
-        addButtonToBoard();
 
-        frame.add(mainPanel);
-        frame.pack();
+        add(mainPanel);
+        pack();
 
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.addWindowListener(new WindowAdapter() {
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        MonopolyGUIView self = this;
+        addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent we) {
-                if (JOptionPane.showConfirmDialog(frame, "Are you sure you want to quit?")
+                if (JOptionPane.showConfirmDialog(self, "Are you sure you want to quit?")
                         == JOptionPane.OK_OPTION) {
-                    frame.setVisible(false);
-                    frame.dispose();
+                    self.setVisible(false);
+                    self.dispose();
                 }
             }
         });
 
-        frame.setVisible(true);
+        self.setVisible(true);
     }
 
     public static void main(String[] args) {
