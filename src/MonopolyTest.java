6fab4cce4050a5de0d60eb5bd1b6b3e1e4a432c7
bobbyngo <ngohuugiabao8980@gmail.com>
2021-11-07 17:00:01 -0500

    test cases

---
+++ src/MonopolyTest.java
@@ -0,0 +1,60 @@
+import org.junit.Before;
+
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+/**
+ * Testing class for monopoly
+ */
+
+public class MonopolyTest {
+    private MonopolyController controller;
+
+    //Player p1 =new Player("player4", new Square("GO", 0));
+
+    @Before
+    public void init() {
+        //For running the code, players array list cannot be empty
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(new Player("player1", new Square("GO", 0)));
+        players.add(new Player("player2", new Square("GO", 0)));
+        players.add(new Player("player3", new Square("GO", 0)));
+        players.add(new Player("player4", new Square("GO", 0)));
+
+        controller = new MonopolyController(players);
+    }
+
+    @org.junit.Test
+    public void testBuyBusiness(){
+        controller.purchaseProperty(new Business("Anarchy Acres", 1, "red",50));
+        ArrayList<PrivateProperty> privatePropertyList = controller.getCurrentPlayer().getPropertyList();
+        assertEquals(1, privatePropertyList.size());
+    }
+
+    @org.junit.Test
+    public void testBuyRail(){
+        controller.purchaseProperty(new Rail("South Rail", 3, 150));
+        ArrayList<PrivateProperty> privatePropertyList = controller.getCurrentPlayer().getPropertyList();
+        assertEquals(1, privatePropertyList.size());
+    }
+
+    @org.junit.Test
+    public void testSellBusiness(){
+
+    }
+
+    @org.junit.Test
+    public void testSellRail(){
+
+    }
+
+    @org.junit.Test
+    public void testPayingTax(){
+
+    }
+
+    @org.junit.Test
+    public void testPlayerMoving(){
+
+    }
+}
